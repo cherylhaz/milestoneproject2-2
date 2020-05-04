84477e022ba8ec7149e4f127db6ff001cac772a7
cherylhaz <cheryl.hazenberg@gmail.com>
2020-05-04 14:18:22 +0000

    Displaying first trail name, location and summary

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,26 +4,27 @@
 //var switzerland = (lat: 46.8182, lon: 8.2275)
 //var spain = (lat: 40.4637, lon: 3.7492)
 
-function trailInformationHTML(trail){
-    return `
-    <h2>${trailOptions.trails.name}</h2>`;
+function trailInformationHTML(trails){
+    return `<span class="trail_name"><h2>${trailOptions.trails[0].name}</h2></span>
+    <span class="trail_info"><p>${trailOptions.trails[0].summary}<br> ${trailOptions.trails[0].location}</p></span>`;
 }
 
 function search(event){
     var inputCountry = $("#country").val();
-    if (!country){
+    if (!inputCountry){
         $("#name").html(`<h2>Please select a country</h2>`);
         return;
-    } if(country = "Ireland"){
+    } if(inputCountry = "Ireland"){
 $.when(
     $.getJSON('https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200735964-55871b76fd696d0af0539bd9bc3b2dd6')
     ).then(
         function(response){
              trailOptions = response;
+             console.log(response);
             $("#name").html(trailInformationHTML(trailOptions));
         }, function(errorResponse){
             if (errorResponse.status === 404){
-                $("#name").html(`<h2>Nothing was found for ${country}</h2>`);
+                $("#name").html(`<h2>Nothing was found for ${inputCountry}</h2>`);
             } else {
                 console.log(errorResponse);
                 $("#name").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
