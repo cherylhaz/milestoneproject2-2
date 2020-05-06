225a6a8569b8fbf9829a196c949703f825b7b077
cherylhaz <cheryl.hazenberg@gmail.com>
2020-05-06 16:50:55 +0000

    Beginning work on country search

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,23 +1,30 @@
-const irelandLat = 53.1424;
-const irelandLon = -6.266155;
-//var italy = (lat: 41.8719, lon: 12.5674)
-//var portugal = (lat: 39.3999, lon: 8.2245)
-//var switzerland = (lat: 46.8182, lon: 8.2275)
-//var spain = (lat: 40.4637, lon: 3.7492)
 
-function search(event){
+const irelandLon = -6.266155;
+//var italy = (lat: , lon: 12.5674)
+//var portugal = (lat: , lon: 8.2245)
+//var switzerland = (lat: , lon: 8.2275)
+//var spain = (lat: , lon: 3.7492)
+function getCountryCoordinates(selectedCountryLat){
+        
+    }
+
+
+function search(event, selectedCountryLat){
     var inputCountry = $("#country").val();
     var inputDistance = $("#distance").val();
     var inputStars = $("#rating").val();
+    var countryLat = [{'name': 'ireland', 'lat': '53.1424'},{'name': 'italy', 'lat': '41.8719'}];
+    var selectedCountryLat = $(countryLat).find(name => name === 'ireland').lat;
+    
     if (!inputCountry){
         $("#name").html(`<h2>Please select a country</h2>`);
         return;
-    } if(inputCountry = "Ireland"){
-$.when(
-   
-    $.getJSON('https://www.hikingproject.com/data/get-trails',
+    } else {
+    
+$.when (
+   $.getJSON('https://www.hikingproject.com/data/get-trails',
         {key: "200735964-55871b76fd696d0af0539bd9bc3b2dd6",
-        lat: 53.1424,
+        lat: selectedCountryLat,
         lon: -6.266155,
         minLength: inputDistance,
         minStars: inputStars
