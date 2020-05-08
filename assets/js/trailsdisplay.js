f54223a197a34b8571b946c25d16e78350ad60ec
cherylhaz <cheryl.hazenberg@gmail.com>
2020-05-08 11:06:20 +0000

    Map zoom working on click, proper labels added, italy coordinates updated

--- assets/js/trailsdisplay.js
+++ assets/js/trailsdisplay.js
@@ -5,23 +5,28 @@ function trailInformationHTML(returnedTrails){
     for (let returnedTrail of returnedTrails) {
 
         var myLatLng = new google.maps.LatLng(`${returnedTrail.latitude}`,`${returnedTrail.longitude}`);
-        var contentString = `<div id="content">${returnedTrail.name}</div>`;
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
+        var label = returnedTrails.indexOf(returnedTrail) +1;
         
         var marker = new google.maps.Marker({
             position: myLatLng,
             map: map,
-            label: `${returnedTrails.indexOf(returnedTrail)}`,
+            label: `${label}`,
         });
-        markers.push(marker);
-
-
+    markers.push(marker);
+    //Code used from W3Schools https://www.w3schools.com/graphics/google_maps_events.asp
+    google.maps.event.addListener(marker,'click',function() {
+  map.setZoom(9);
+  map.setCenter(marker.getPosition());
+});
+//end of Code
     marker.addListener('click', function() {
     infowindow.open(map, marker);
   });
-    document.getElementById("search-results").innerHTML += `<span id ="number">${returnedTrails.indexOf(returnedTrail)} <a href="${returnedTrail.url}" target="_blank">${returnedTrail.name}   </a></span>
+  var contentString = `<div id="content">${returnedTrail.name}</div>`;
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        }); 
+    document.getElementById("search-results").innerHTML += `<span id ="number">${label} <a href="${returnedTrail.url}" target="_blank">${returnedTrail.name}   </a></span>
                                             <span id="summary">${returnedTrail.summary} </span>
                                             <span><i class="fas fa-arrows-alt-h"></i> ${returnedTrail.length} miles  </span>
                                             <span><i class="fas fa-arrow-up"></i> ${returnedTrail.ascent} m  </span>
@@ -31,7 +36,7 @@ function trailInformationHTML(returnedTrails){
                                         }
 }
 
-
+//Code from Google maps documentation https://developers.google.com/maps/documentation/javascript/examples/marker-remove
 function setMapOnAll(map) {
         for (var i = 0; i < markers.length; i++) {
           markers[i].setMap(map);
@@ -48,3 +53,4 @@ function deleteMarkers(){
 function clearMarkers() {
         setMapOnAll(null);
       }
+//End of Google Code
