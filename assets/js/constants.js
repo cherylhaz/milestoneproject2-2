45d6d648de399bb2de8c4b5b54a70e02e0e0e5f5
cherylhaz <cheryl.hazenberg@gmail.com>
2020-05-13 08:57:18 +0000

    country data moved to its own file

---
+++ assets/js/constants.js
@@ -0,0 +1,38 @@
+const countries = [{
+    'name': 'Dublin,Ireland',
+    'lat': '53.1424',
+    'lon': '-6.266155'
+}, {
+    'name': 'Riomaggiore,Italy',
+    'lat': '44.1',
+    'lon': '9.75'
+}, {
+    'name': 'Lisbon,Portugal',
+    'lat': '38.736946',
+    'lon': '-9.142685'
+}, {
+    'name': 'Amsterdam,Netherlands',
+    'lat': '52.370216',
+    'lon': '4.895168'
+}, {
+    'name': 'Madrid,Spain',
+    'lat': '40.416775',
+    'lon': '-3.703790'
+}, {
+    'name': 'Barcelona,Spain',
+    'lat': '41.3948976',
+    'lon': '2.0787279'
+}, {
+    'name': 'Porto,Portugal',
+    'lat': '41.1622023',
+    'lon': '-8.6569731'
+},
+{
+    'name': 'Mayo,Ireland',
+    'lat': '54.2245815',
+    'lon': '-9.9918944'
+}, {
+    'name': 'Geneva,Switzerland',
+    'lat': '46.204391',
+    'lon': '6.143158'
+}];
